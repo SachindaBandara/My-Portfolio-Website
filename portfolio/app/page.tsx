6578cccb8d5db2a3915a40d6e32dab8e1e7c4cb6
SachindaUB <jmsubandara@std.appsc.sab.ac.lk>
2024-09-30 17:32:30 +0530

    create nav bar and bento grid

--- portfolio/app/page.tsx
+++ portfolio/app/page.tsx
@@ -1,11 +1,18 @@
+import Grid from "@/components/Grid";
 import Hero from "@/components/Hero";
+import { FloatingNav } from "@/components/ui/FloatingNavbar";
 import React from "react";
+import { FaHome } from "react-icons/fa";
 
 function Home() {
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-1 px-5">
       <div className="max-w-7xl w-full">
+        <FloatingNav navItems={[
+            {name: "Home", link: "/", icon: <FaHome/>}
+        ]}/>
         <Hero />
+        <Grid/>
       </div>
     </main>
   );
