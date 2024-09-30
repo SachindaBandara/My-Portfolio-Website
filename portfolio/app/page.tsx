743f3ddf051a416614795c9088cb657cbd72bc1f
SachindaUB <jmsubandara@std.appsc.sab.ac.lk>
2024-09-30 15:29:25 +0530

    create spotlight in the hero section

--- portfolio/app/page.tsx
+++ portfolio/app/page.tsx
@@ -1,10 +1,11 @@
+import Hero from "@/components/Hero";
 import React from "react";
 
 function Home() {
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-1 px-5">
       <div className="max-w-7xl w-full">
-        <h1>Hello</h1>
+        <Hero />
       </div>
     </main>
   );
