beb1a3e407ddadb4bbf5510f71656518408b7228
SachindaUB <jmsubandara@std.appsc.sab.ac.lk>
2024-09-30 19:33:33 +0530

    add

--- portfolio/components/Grid.tsx
+++ portfolio/components/Grid.tsx
@@ -1,11 +1,22 @@
-import React from 'react'
+import React from "react";
+import { BentoGrid, BentoGridItem } from "./ui/BentoGrid";
+import { gridItems } from "@/data";
 
 const Grid = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <section id="about">
+      <BentoGrid>
+        {gridItems.map((item) => (
+          <BentoGridItem
+            id={item.id}
+            key={item.id}
+            title={item.title}
+            description={item.description}
+          />
+        ))}
+      </BentoGrid>
+    </section>
+  );
+};
 
-export default Grid
+export default Grid;
---
+++ portfolio/data/index.tsx
@@ -0,0 +1,6 @@
+export const gridItems = [
+    { title: "Title1", description: "Desc1", id: 1, },
+    { title: "Title1", description: "Desc1", id: 1, },
+    { title: "Title1", description: "Desc1", id: 1, },
+    { title: "Title1", description: "Desc1", id: 1, },
+]
