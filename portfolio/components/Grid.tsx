a949349779910ee79431953af137a10df31f9b91
SachindaUB <jmsubandara@std.appsc.sab.ac.lk>
2024-09-30 21:13:50 +0530

    add bento grid

--- portfolio/components/Grid.tsx
+++ portfolio/components/Grid.tsx
@@ -6,12 +6,18 @@ const Grid = () => {
   return (
     <section id="about">
       <BentoGrid>
-        {gridItems.map((item) => (
+        {gridItems.map(({ id, title, description, className, img, imgClassName, titleClassName, spareImg}) => (
           <BentoGridItem
-            id={item.id}
-            key={item.id}
-            title={item.title}
-            description={item.description}
+            id={id}
+            key={id}
+            title={title}
+            description={description}
+            className={className}
+            img={img}
+            imgClassName={imgClassName}
+            titleClassName={titleClassName}
+            spareImg={spareImg}
+
           />
         ))}
       </BentoGrid>
