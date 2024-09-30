1abedaaf62c67221abff030c9f5ed2e9ef135486
SachindaUB <jmsubandara@std.appsc.sab.ac.lk>
2024-09-30 15:12:51 +0530

    create page layout

---
+++ portfolio/app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import "./globals.css";
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Sachinda Bandara",
+  description: "Sachinda Bandara personal website",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" className="!scroll-smooth">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
---
+++ portfolio/app/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Home() {
+  return (
+    <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-1 px-5">
+      <div className="max-w-7xl w-full">
+        <h1>Hello</h1>
+      </div>
+    </main>
+  );
+}
+
+export default Home;
