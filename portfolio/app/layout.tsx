f3fd134f251022047426ea3b0e8bfecada604304
SachindaUB <jmsubandara@std.appsc.sab.ac.lk>
2024-09-30 15:50:48 +0530

    add dark mode

--- portfolio/app/layout.tsx
+++ portfolio/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import { Inter } from "next/font/google";
+import { ThemeProvider } from "./Provider";
+
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +18,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="!scroll-smooth">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}><ThemeProvider
+            attribute="class"
+            defaultTheme="dark"
+            enableSystem
+            disableTransitionOnChange
+          >
+            {children}
+          </ThemeProvider></body>
     </html>
   );
 }
