596d971680d5eba5553e1cbabcd013f4138a7006
SachindaUB <jmsubandara@std.appsc.sab.ac.lk>
2024-10-01 11:57:14 +0530

    add

--- portfolio/components/Footer.tsx
+++ portfolio/components/Footer.tsx
@@ -7,13 +7,6 @@ import { profile } from "console";
 const Footer = () => {
   return (
     <footer className="w-full pt-20 pb-10" id="contact">
-      <div className="w-full absolute left-0 -bottom-72 min-h-96">
-        <img
-          src="/footer-gird.svg"
-          alt="grid"
-          className="w-full h-full opacity-50"
-        />
-      </div>
 
       <div className="flex flex-col items-center">
         <h1 className="heading lg:max-w-[45vw]">
