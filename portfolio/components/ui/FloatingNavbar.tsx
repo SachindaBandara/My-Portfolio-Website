1df154c33ec9021d2e3d889cdec899b4d7153738
SachindaUB <jmsubandara@std.appsc.sab.ac.lk>
2024-10-04 23:31:45 +0530

    fix navbar

--- portfolio/components/ui/FloatingNavbar.tsx
+++ portfolio/components/ui/FloatingNavbar.tsx
@@ -20,41 +20,8 @@ export const FloatingNav = ({
   }[];
   className?: string;
 }) => {
-  const { scrollYProgress } = useScroll();
-
-  const [visible, setVisible] = useState(false);
-
-  useMotionValueEvent(scrollYProgress, "change", (current) => {
-    if (typeof current === "number") {
-      let direction = current! - scrollYProgress.getPrevious()!;
-
-      if (scrollYProgress.get() < 0.05) {
-        setVisible(false);
-      } else {
-        if (direction < 0) {
-          setVisible(true);
-        } else {
-          setVisible(false);
-        }
-      }
-    }
-  });
-
-  return (
-    <AnimatePresence mode="wait">
-      <motion.div
-        initial={{
-          opacity: 1,
-          y: -100,
-        }}
-        animate={{
-          y: visible ? 0 : -100,
-          opacity: visible ? 1 : 0,
-        }}
-        transition={{
-          duration: 0.2,
-        }}
-        className={cn(
+ return (
+    <div className={cn(
           "flex max-w-fit fixed top-10 inset-x-0 mx-auto border rounded-lg shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)] z-[5000] px-10 py-5 items-center justify-center space-x-4 border-white/[0.2] bg-black-100",
           className
         )}
@@ -72,7 +39,7 @@ export const FloatingNav = ({
           </Link>
         ))}
        
-      </motion.div>
-    </AnimatePresence>
+      </div>
+ 
   );
 };
