d084776b27fc2cf2ad7352571f94df6b474f0acc
SachindaUB <jmsubandara@std.appsc.sab.ac.lk>
2024-10-14 22:21:18 +0530

    add

--- portfolio/components/ui/FloatingNavbar.tsx
+++ portfolio/components/ui/FloatingNavbar.tsx
@@ -20,26 +20,26 @@ export const FloatingNav = ({
   }[];
   className?: string;
 }) => {
- return (
+  return (
     <div className={cn(
-          "flex max-w-fit fixed top-10 inset-x-0 mx-auto border rounded-lg shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)] z-[5000] px-10 py-5 items-center justify-center space-x-4 border-white/[0.2] bg-black-100",
-          className
-        )}
-      >
-        {navItems.map((navItem: any, idx: number) => (
-          <Link
-            key={`link=${idx}`}
-            href={navItem.link}
-            className={cn(
-              "relative dark:text-neutral-50 items-center flex space-x-1 text-neutral-600 dark:hover:text-neutral-300 hover:text-neutral-500"
-            )}
-          >
-            <span className="block sm:hidden">{navItem.icon}</span>
-            <span className="text-sm !cursor-pointer">{navItem.name}</span>
-          </Link>
-        ))}
-       
-      </div>
- 
+      "flex max-w-fit fixed top-10 inset-x-0 mx-auto border rounded-lg shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)] z-[5000] px-10 py-5 items-center justify-center space-x-4 border-white/[0.2] bg-black-100",
+      className
+    )}
+    >
+      {navItems.map((navItem: any, idx: number) => (
+        <Link
+          key={`link=${idx}`}
+          href={navItem.link}
+          className={cn(
+            "relative dark:text-neutral-50 items-center flex space-x-1 text-neutral-600 dark:hover:text-neutral-300 hover:text-neutral-500"
+          )}
+        >
+          <span className="block sm:hidden">{navItem.icon}</span>
+          <span className="text-sm !cursor-pointer">{navItem.name}</span>
+        </Link>
+      ))}
+
+    </div>
+
   );
 };
